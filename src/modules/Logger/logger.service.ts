95192de5b0de1c68d83396b3346ab40792a0caf0
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-15 15:03:47 -0400

    feat: Add LoggerService for centralized logging

---
+++ src/modules/Logger/logger.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
+export class LoggerService {
+  private readonly logger = new Logger();
+
+  log(message: string) {
+    this.logger.log(message);
+  }
+
+  error(message: string, trace?: string) {
+    this.logger.error(message, trace);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+}
