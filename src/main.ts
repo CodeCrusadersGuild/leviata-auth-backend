14641afce367d910bc1a2f2f798e94ef9203e88b
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-15 19:14:59 -0400

    refactor: refactor logging to use Winston for improved log management

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { readFileSync } from 'fs';
-import { Logger } from '@nestjs/common';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: new Logger(),
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json(),
+    ),
+    transports: [new winston.transports.Console()],
   });
+
+  const app = await NestFactory.create(AppModule, { logger });
 
   const packageJson = JSON.parse(readFileSync('package.json', 'utf8'));
   const version = packageJson.version;
