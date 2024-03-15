20ed98aaf06953ed34debb22aef4dedcd3d00b83
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-15 15:05:54 -0400

    feat: Integrate LoggerService for logging

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './modules/Logger/logger.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly logger: LoggerService) {}
+
   getHello(): string {
+    this.logger.log('Obtendo exemplo...');
     return 'Hello World!';
   }
 }
