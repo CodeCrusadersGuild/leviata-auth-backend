9ea4799238a1416ab5c0b1b22614e753791206ae
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-15 19:25:41 -0400

    refactor: refactor LoggerService to utilize winston for enhanced logging capabilities

--- src/modules/Logger/logger.service.ts
+++ src/modules/Logger/logger.service.ts
@@ -1,22 +1,64 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import * as winston from 'winston';
 
 @Injectable()
 export class LoggerService {
-  private readonly logger = new Logger();
+  private readonly logger: winston.Logger;
 
-  log(message: string) {
-    this.logger.log(message);
+  constructor() {
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      transports: [new winston.transports.Console()],
+    });
   }
 
-  error(message: string, trace?: string) {
-    this.logger.error(message, trace);
+  private logWithLevel(
+    level: string,
+    messageObject: { message: string; context?: string; timestamp?: string },
+  ) {
+    this.logger.log({
+      level,
+      message: messageObject.message,
+      context: messageObject.context,
+      timestamp: messageObject.timestamp,
+    });
   }
 
-  warn(message: string) {
-    this.logger.warn(message);
+  log(messageObject: {
+    message: string;
+    context?: string;
+    timestamp?: string;
+  }) {
+    this.logWithLevel('info', messageObject);
   }
 
-  debug(message: string) {
-    this.logger.debug(message);
+  error(
+    messageObject: { message: string; context?: string; timestamp?: string },
+    trace?: string,
+  ) {
+    this.logWithLevel('error', messageObject);
+    if (trace) {
+      this.logger.error(trace);
+    }
+  }
+
+  warn(messageObject: {
+    message: string;
+    context?: string;
+    timestamp?: string;
+  }) {
+    this.logWithLevel('warn', messageObject);
+  }
+
+  debug(messageObject: {
+    message: string;
+    context?: string;
+    timestamp?: string;
+  }) {
+    this.logWithLevel('debug', messageObject);
   }
 }
