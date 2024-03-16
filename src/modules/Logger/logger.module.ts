c86aafd5ec37ef1784c20750b3cca8cbe2220096
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-16 19:58:48 -0400

    feat: add LocalStorageLoggerService to providers in LoggerModule

--- src/modules/Logger/logger.module.ts
+++ src/modules/Logger/logger.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { LocalStorageLoggerService } from './local.storage.logger.context.service';
 
 @Module({
-  providers: [],
+  providers: [LocalStorageLoggerService],
   exports: [],
 })
 export class LoggerModule {}
