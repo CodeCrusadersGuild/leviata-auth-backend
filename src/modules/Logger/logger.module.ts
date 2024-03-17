ab701ce451ea9e48da2abccffde843be927cf7e1
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-16 20:17:50 -0400

    refactor: refactor LoggerModule to Include LoggerContextService

--- src/modules/Logger/logger.module.ts
+++ src/modules/Logger/logger.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LocalStorageLoggerService } from './local.storage.logger.context.service';
+import { LoggerContextService } from './logger.context.service';
 
 @Module({
-  providers: [LocalStorageLoggerService],
+  providers: [LocalStorageLoggerService, LoggerContextService],
   exports: [],
 })
 export class LoggerModule {}
