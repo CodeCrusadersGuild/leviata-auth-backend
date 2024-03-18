fbbe272f1ba6af12308c498928cbfe2c730934f9
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-17 21:03:50 -0400

    refactor: refactor caller service to encapsulate module information retrieval logic

--- src/modules/Logger/services/caller.service.ts
+++ src/modules/Logger/services/caller.service.ts
@@ -9,6 +9,12 @@ import { OriginFunction } from '../types/origin.function.type';
 export class CallerService {
   constructor(private readonly stackTraceService: StackTraceService) {}
 
+  /**
+   * Retrieves information about the module that called a specified method at a given stack trace level.
+   * @param method The method whose caller module information is to be determined.
+   * @param level The stack trace level at which the caller module is to be determined. Defaults to 1.
+   * @returns A promise that resolves to an object containing the name, root, and path of the caller module.
+   */
   async getCallerModule(
     method: OriginFunction,
     level?: number,
@@ -18,6 +24,11 @@ export class CallerService {
     return this.resolveModuleInfo(caller);
   }
 
+  /**
+   * Resolves module information for the provided call site.
+   * @param caller The call site for which module information is to be resolved.
+   * @returns A promise that resolves to an object containing the name, root, and path of the module.
+   */
   private async resolveModuleInfo(
     caller: CallSite,
   ): Promise<{ name: string; root: string; path: string }> {
@@ -31,6 +42,11 @@ export class CallerService {
     };
   }
 
+  /**
+   * Retrieves the name of the module based on the provided file path.
+   * @param callerPath The file path of the caller.
+   * @returns A promise that resolves to the name of the module.
+   */
   private async getModuleName(callerPath: string): Promise<string> {
     if (Path.basename(callerPath) === callerPath) {
       return callerPath;
@@ -44,6 +60,11 @@ export class CallerService {
     }
   }
 
+  /**
+   * Finds the root directory of the module based on the provided file path.
+   * @param callerPath The file path of the caller.
+   * @returns A promise that resolves to the root directory of the module.
+   */
   private async findModuleRoot(callerPath: string): Promise<string> {
     let root = callerPath;
     let oldRoot = null;
@@ -54,6 +75,11 @@ export class CallerService {
     return root;
   }
 
+  /**
+   * Determines whether the provided directory is the root of a module.
+   * @param fileName The name of the directory to be checked.
+   * @returns A boolean indicating whether the directory is the root of a module.
+   */
   private isModuleRoot(fileName: string): boolean {
     const files = FileSystem.readdirSync(fileName).filter(
       value => !FileSystem.lstatSync(Path.join(fileName, value)).isDirectory(),
@@ -61,6 +87,11 @@ export class CallerService {
     return files.includes('package.json');
   }
 
+  /**
+   * Reads the package name from the package.json file located in the module root.
+   * @param moduleRoot The root directory of the module.
+   * @returns A promise that resolves to the name of the module.
+   */
   private async readPackageName(moduleRoot: string): Promise<string> {
     const packageJsonPath = Path.join(moduleRoot, 'package.json');
     const packageJsonContent = FileSystem.readFileSync(packageJsonPath, 'utf8');
@@ -68,6 +99,11 @@ export class CallerService {
     return packageJson.name;
   }
 
+  /**
+   * Extracts the module name from the node_modules path.
+   * @param callerPath The file path of the caller.
+   * @returns The module name extracted from the node_modules path.
+   */
   private extractNodeModuleName(callerPath: string): string {
     const pathTree = callerPath.split(Path.sep);
     const moduleFolderIndex = pathTree.indexOf('node_modules') + 1;
