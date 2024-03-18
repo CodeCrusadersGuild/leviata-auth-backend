591e0bb95b494a51ced2152a22c02cb4a0a68600
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-18 17:18:12 -0400

    feat: create Logger enums for LogActions and LogLevel

---
+++ src/modules/Logger/enums/log.actions.ts
@@ -0,0 +1,10 @@
+export enum LogActions {
+  MethodTrace = 'method-trace',
+  Request = 'request',
+  Response = 'response',
+  Integration = 'integration',
+  SlsExecution = 'sls-execution',
+  SlsExecutionError = 'sls-execution-error',
+  ControllerRegister = 'controller-register',
+  NewEvent = 'event',
+}
