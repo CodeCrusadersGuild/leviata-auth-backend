98f907bb7cf4251e7f354f35a252011190e687c8
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-17 20:58:40 -0400

    refactor: refactor stack trace service for better configurability and readability

--- src/modules/Logger/services/stack.trace.service.ts
+++ src/modules/Logger/services/stack.trace.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CallSite } from 'callsites';
-
-type OriginFunction = (...args: any[]) => any;
-type TraceFunction = (err: Error, stack: CallSite[]) => any;
+import { TraceFunction } from '../types/trace.function.type';
+import { OriginFunction } from '../types/origin.function.type';
 
 @Injectable()
 export class StackTraceService {
@@ -24,25 +23,48 @@ export class StackTraceService {
     frames: number,
     origin?: OriginFunction,
   ): Promise<CallSite[]> {
-    // Set default values if parameters are not provided
     origin = origin || this.getCallsites;
     frames = Math.abs(Math.floor(frames)) || 1;
     origin = typeof origin === 'function' && origin;
 
-    // Configure stack trace limit and trace function
+    this.configureStackTrace(frames, origin);
+
+    const stack = await this.captureStackTrace(origin);
+
+    this.restoreOriginalStackTrace();
+
+    return stack;
+  }
+
+  /**
+   * Configures the stack trace limit and trace function.
+   * @param frames The number of frames.
+   * @param origin The origin function for the stack trace.
+   */
+  private configureStackTrace(frames: number, origin?: OriginFunction): void {
     Error.stackTraceLimit = origin ? frames : frames + 1;
     Error.prepareStackTrace = (_, stack) => stack;
+  }
 
-    // Capture stack trace
+  /**
+   * Captures the stack trace.
+   * @param origin The origin function for the stack trace.
+   * @returns A promise that resolves to an array of call sites.
+   */
+  private async captureStackTrace(
+    origin: OriginFunction | undefined,
+  ): Promise<CallSite[]> {
     const error = new Error();
     Error.captureStackTrace(error, origin || this.getCallsites);
     const stack = origin ? error.stack : error.stack.slice(1);
+    return stack as unknown as CallSite[];
+  }
 
-    // Restore original stack trace limit and trace function
+  /**
+   * Restores the original stack trace limit and trace function.
+   */
+  private restoreOriginalStackTrace(): void {
     Error.stackTraceLimit = this.originalLimit;
     Error.prepareStackTrace = this.originalTrace;
-
-    // Return the stack as call sites
-    return stack as unknown as CallSite[];
   }
 }
