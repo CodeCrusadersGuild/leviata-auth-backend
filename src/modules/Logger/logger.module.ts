8dfb6b257b13ad3d1d96e24780887ff78630caf2
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-17 21:14:24 -0400

    refactor: refactor Logger module to include new services for improved logging capabilities

--- src/modules/Logger/logger.module.ts
+++ src/modules/Logger/logger.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { CallerService } from './services/caller.service';
 import { LocalStorageService } from './services/local.storage.service';
+import { LoggerBuilderService } from './services/logger.builder.service';
 import { LoggerContextService } from './services/logger.context.service';
+import { StackTraceService } from './services/stack.trace.service';
 
 @Module({
-  providers: [LocalStorageService, LoggerContextService],
+  providers: [
+    CallerService,
+    LocalStorageService,
+    LoggerBuilderService,
+    LoggerContextService,
+    StackTraceService,
+  ],
   exports: [],
 })
 export class LoggerModule {}
