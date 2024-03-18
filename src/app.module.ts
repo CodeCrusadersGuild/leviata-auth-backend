824398464b678b06977c5c8381465babb1029506
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-18 17:47:56 -0400

    refactor: Refactor request logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './modules/Logger/logger.module';
-import { RequestLoggingMiddleware } from './modules/logger/middlewares/request.logging.middleware';
 
 @Module({
   imports: [LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
--- src/modules/Logger/logger.module.ts
+++ src/modules/Logger/logger.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CallerService } from './services/caller.service';
 import { LocalStorageService } from './services/local.storage.service';
 import { LoggerBuilderService } from './services/logger.builder.service';
@@ -9,19 +9,23 @@ import { RequestLoggingMiddleware } from './middlewares/request.logging.middlewa
 
 @Module({
   providers: [
+    RequestLoggingMiddleware,
     CallerService,
     LocalStorageService,
     LoggerBuilderService,
     LoggerContextService,
     StackTraceService,
     LoggerService,
-    RequestLoggingMiddleware,
   ],
   exports: [
-    LoggerService,
     RequestLoggingMiddleware,
+    LoggerService,
     LocalStorageService,
     LoggerContextService,
   ],
 })
-export class LoggerModule {}
+export class LoggerModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
+  }
+}
