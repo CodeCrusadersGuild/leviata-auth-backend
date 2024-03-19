ce6cb95f2fa75955d8df5e40929f0258bc0aa694
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-19 01:41:56 -0400

    refactor: Refactor LoggerModule providers

--- src/modules/Logger/logger.module.ts
+++ src/modules/Logger/logger.module.ts
@@ -1,28 +1,10 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { CallerService } from './services/caller.service';
-import { LocalStorageService } from './services/local.storage.service';
-import { LoggerBuilderService } from './services/logger.builder.service';
-import { LoggerContextService } from './services/logger.context.service';
-import { StackTraceService } from './services/stack.trace.service';
 import { LoggerService } from './services/logger.service';
 import { RequestLoggingMiddleware } from './middlewares/request.logging.middleware';
 
 @Module({
-  providers: [
-    RequestLoggingMiddleware,
-    CallerService,
-    LocalStorageService,
-    LoggerBuilderService,
-    LoggerContextService,
-    StackTraceService,
-    LoggerService,
-  ],
-  exports: [
-    RequestLoggingMiddleware,
-    LoggerService,
-    LocalStorageService,
-    LoggerContextService,
-  ],
+  providers: [RequestLoggingMiddleware, LoggerService],
+  exports: [RequestLoggingMiddleware, LoggerService],
 })
 export class LoggerModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
