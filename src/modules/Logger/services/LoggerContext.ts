bf2b6218b204855530984c05e92e9979b3b12575
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-19 01:40:39 -0400

    refactor: Refactor Logger Context Service to Static Methods

--- src/modules/Logger/services/LoggerContext.ts
+++ src/modules/Logger/services/LoggerContext.ts
@@ -1,18 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { LocalStorageService } from './local.storage.service';
+import { LocalStorage } from './LocalStorage';
 import { ContextNamespace } from '../enums/context-namespaces';
 import { LoggerConfigs } from '../config/logger-configs';
 
-@Injectable()
-export class LoggerContextService {
-  constructor(private readonly localStorageService: LocalStorageService) {}
-
+export class LoggerContext {
   /**
    * Sets the correlation ID in the logger context.
    * @param correlationId The correlation ID to set.
    */
-  public setCorrelationId(correlationId: string): void {
-    this.localStorageService.setContextValue(
+  static setCorrelationId(correlationId: string): void {
+    LocalStorage.setContextValue(
       'correlationId',
       correlationId,
       ContextNamespace.LOGGER,
@@ -23,8 +19,8 @@ export class LoggerContextService {
    * Retrieves the correlation ID from the logger context.
    * @returns The correlation ID.
    */
-  public getCorrelationId(): string {
-    return this.localStorageService.getContextValue(
+  static getCorrelationId(): string {
+    return LocalStorage.getContextValue(
       'correlationId',
       ContextNamespace.LOGGER,
     ) as string;
@@ -35,20 +31,16 @@ export class LoggerContextService {
    * @param key The key of the data.
    * @param value The value of the data.
    */
-  public setLogInfoData(key: string, value: unknown): void {
-    this.localStorageService.setContextValue(
-      key,
-      value,
-      ContextNamespace.LOGGER,
-    );
+  static setLogInfoData(key: string, value: unknown): void {
+    LocalStorage.setContextValue(key, value, ContextNamespace.LOGGER);
   }
 
   /**
    * Retrieves additional log info data from the logger context.
    * @returns The additional log info data object.
    */
-  public getLogInfoData(): object {
-    return this.localStorageService.getContextValue(
+  static getLogInfoData(): object {
+    return LocalStorage.getContextValue(
       'extraLogInfo',
       ContextNamespace.LOGGER,
     ) as object;
@@ -58,7 +50,7 @@ export class LoggerContextService {
    * Adds fields to the list of hidden fields in logs.
    * @param fields The fields to add to the list of hidden fields.
    */
-  public static addLoggerHiddenField(fields: Array<string>): void {
+  static addLoggerHiddenField(fields: Array<string>): void {
     fields = fields || [];
     LoggerConfigs.hiddenFields = LoggerConfigs.hiddenFields.concat(fields);
   }
@@ -67,7 +59,7 @@ export class LoggerContextService {
    * Sets the list of hidden fields in logs.
    * @param fields The fields to set as hidden in logs.
    */
-  public setLoggerHiddenField(fields: string[]): void {
+  static setLoggerHiddenField(fields: string[]): void {
     fields = fields || [];
     LoggerConfigs.hiddenFields = fields;
   }
