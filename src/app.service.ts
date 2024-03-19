be56a866f0573dc92562347da2d443c91b710047
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-19 02:50:31 -0400

    refactor: Refactor logging in getHello method

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-// import { LoggerService } from './shared/logger/services/LoggerService';
+import { LoggerService } from './shared/logger/services/LoggerService';
 import { log } from './shared/logger/decorators/log-decorator';
 
 @Injectable()
 export class AppService {
   @log()
   getHello(): string {
-    // LoggerService.info('Olá família');
-    // LoggerService.info('Olá família 2');
+    LoggerService.info('Olá família');
+    LoggerService.info('Olá família 2');
     return 'Hello World!';
   }
 }
