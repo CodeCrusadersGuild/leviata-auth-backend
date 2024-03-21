56a4890a602875bc85496870f92a092175697016
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-21 17:02:50 -0400

    refactor: Refactor request interceptor to log response details

--- src/interceptors/request.interceptor.ts
+++ src/interceptors/request.interceptor.ts
@@ -8,11 +8,14 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { LoggerService } from 'src/shared/logger/LoggerService';
 import { ApiResponse } from 'src/shared/types/api.response';
+import { ResponseLog } from 'src/shared/logger/types/response.log.types';
 
 @Injectable()
 export class RequestInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
+
+    const startTime = Date.now();
 
     const requestData = {
       method: request.method,
@@ -30,7 +33,22 @@ export class RequestInterceptor implements NestInterceptor {
           return;
         }
 
-        LoggerService.debug('[RequestInterceptor] Response Data', response);
+        const duration = Date.now() - startTime;
+
+        const responseLog: ResponseLog = {
+          method: request.method,
+          path: request.url,
+          route: request.route.path,
+          agent: request.get('user-agent'),
+          ip: request.ip,
+          duration: duration,
+          statusCode: response.statusCode,
+          query: request.query,
+          params: request.params,
+          data: response.data,
+        };
+
+        LoggerService.debug('[RequestInterceptor] Response Data', responseLog);
       }),
     );
   }
