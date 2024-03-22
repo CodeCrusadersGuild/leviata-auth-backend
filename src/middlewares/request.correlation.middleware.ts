6770ccca613b4ca703fa0e531106a2c8942fde20
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-22 00:17:02 -0400

    refactor: Refactor request correlation middleware for improved correlation ID handling

--- src/middlewares/request.correlation.middleware.ts
+++ src/middlewares/request.correlation.middleware.ts
@@ -9,17 +9,32 @@ import { LoggerContext } from 'src/shared/logger/LoggerContext';
 export class RequestCorrelationMiddleware implements NestMiddleware {
   constructor() {}
 
+  /**
+   * Middleware function to handle correlation ID generation and storage.
+   * @param req The HTTP request object.
+   * @param res The HTTP response object.
+   * @param next The next function to be called in the middleware chain.
+   */
   async use(req: Request, res: Response, next: NextFunction) {
     await LocalStorage.runInContext(ContextNamespace.LOGGER, async () => {
-      await this.setCorrelationId(req);
-      next();
+      await this.setCorrelationId(req); // Set correlation ID for the request
+      next(); // Call the next middleware or route handler
     });
   }
 
+  /**
+   * Sets the correlation ID for the incoming request.
+   * @param req The HTTP request object.
+   */
   private async setCorrelationId(req: Request): Promise<void> {
+    // Generate correlation ID or use the one provided in the request header
     const correlationId = req.headers['correlationId'] || uuid();
+
+    // Set correlation ID in request headers and properties
     req.headers['correlationId'] = correlationId;
     req['correlationId'] = correlationId;
+
+    // Set correlation ID in logger context for logging purposes
     LoggerContext.setCorrelationId(correlationId.toString());
   }
 }
