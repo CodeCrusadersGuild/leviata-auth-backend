14b3893e29651773c347d1f6b4793ea73774cd7f
Edilson R Lima <edilson.lima@meta.com.br>
2024-03-22 00:02:02 -0400

    refactor: Refactor method decorators to include log levels

--- src/shared/logger/decorators/log-decorator.ts
+++ src/shared/logger/decorators/log-decorator.ts
@@ -2,13 +2,25 @@ import { LogLevel } from '../enums/log.level';
 import { GenericLogDecorator } from './generic-log-decorator';
 
 class LogDecorator extends GenericLogDecorator {
+  /**
+   * Method decorator to add DEBUG level logs.
+   * @returns A method decorator that adds a DEBUG level log.
+   */
   log = () => (target: any, key: PropertyKey, descriptor: PropertyDescriptor) =>
     LogDecorator.apply(key, descriptor, LogLevel.DEBUG);
 
+  /**
+   * Method decorator to add INFO level logs.
+   * @returns A method decorator that adds an INFO level log.
+   */
   info =
     () => (target: any, key: PropertyKey, descriptor: PropertyDescriptor) =>
       LogDecorator.apply(key, descriptor, LogLevel.INFO);
 
+  /**
+   * Method decorator to add WARN level logs.
+   * @returns A method decorator that adds a WARN level log.
+   */
   warn =
     () => (target: any, key: PropertyKey, descriptor: PropertyDescriptor) =>
       LogDecorator.apply(key, descriptor, LogLevel.WARN);
@@ -19,4 +31,5 @@ const decorator: LogDecorator = new LogDecorator();
 const { log } = decorator;
 const { info } = decorator;
 const { warn } = decorator;
+
 export { log, info, warn };
