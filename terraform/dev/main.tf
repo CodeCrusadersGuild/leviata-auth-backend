2a8bbc1f14fca203250caae9cc929b54c6fb14b7
Edilson R Lima <edilson.lima@meta.com.br>
2024-04-20 21:29:35 -0400

    refactor: Refactor Google Cloud services configuration for dev environment

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,30 +1,32 @@
 provider "google" {
   project = var.dev_project_id
   region  = "us-central1"
-  zone    = "us-central1-a"
 }
 
-resource "google_project" "leviata_api_project" {
-  name            = var.dev_project_name
-  project_id      = var.dev_project_id
+resource "google_project_service" "cloud_resource_manager" {
+  project = var.dev_project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+# Crie o bucket no Google Cloud Storage para armazenar os arquivos de estado do ambiente de desenvolvimento
+resource "google_storage_bucket" "terraform_state_dev" {
+  name     = "terraform-state-dev"
+  location = "US" # Defina a localização desejada para o bucket (por exemplo, "US" para os EUA)
+
+  # Adicione outras configurações do bucket, se necessário
 }
 
 resource "google_project_service" "cloud_functions" {
-  project = google_project.leviata_api_project.project_id
+  project = var.dev_project_id
   service = "cloudfunctions.googleapis.com"
 }
 
 resource "google_project_service" "cloud_build" {
-  project = google_project.leviata_api_project.project_id
+  project = var.dev_project_id
   service = "cloudbuild.googleapis.com"
 }
 
-resource "google_project_service" "cloud_storage" {
-  project = google_project.leviata_api_project.project_id
-  service = "storage.googleapis.com"
-}
-
 resource "google_project_service" "cloud_logging" {
-  project = google_project.leviata_api_project.project_id
+  project = var.dev_project_id
   service = "logging.googleapis.com"
 }
