96bac25295615733d8243442259f35d270acffae
Edilson R Lima <edilson.lima@meta.com.br>
2024-04-20 22:23:02 -0400

    refactor: Add dev bucket_name variable for storing Terraform state in GCP.

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -3,3 +3,9 @@ variable "dev_project_id" {
   type        = string
   default     = "leviata-api-dev-421000"
 }
+
+variable "bucket_name" {
+  description = "Nome do bucket do Google Cloud Storage para armazenar os arquivos de estado do Terraform"
+  type        = string
+  default     = "leviata-api-terraform-state-dev"
+}
