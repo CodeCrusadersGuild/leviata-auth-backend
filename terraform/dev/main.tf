6704d58fdb3474e18e3404cb425e4358982a8290
Edilson R Lima <edilson.lima@meta.com.br>
2024-04-28 13:54:26 -0400

    refactor: Refactor Firebase configuration and enable email/password authentication

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,18 +3,6 @@ provider "google" {
   region  = "us-central1"
 }
 
-module "firebase_project" {
-  source          = "terraform-google-modules/project-factory/google"
-  version         = "4.0.0"
-  project_id      = var.dev_project_id
-  name            = null
-  org_id          = null
-  billing_account = null
-  activate_apis   = [
-    "auth.googleapis.com"
-  ]
-}
-
 resource "google_project_service" "cloud_resource_manager" {
   project = var.dev_project_id
   service = "cloudresourcemanager.googleapis.com"
@@ -43,4 +31,20 @@ resource "google_project_service" "deployment_manager" {
 resource "google_project_service" "cloud_logging" {
   project = var.dev_project_id
   service = "logging.googleapis.com"
+}
+
+provider "firebase" {
+  project = var.dev_project_id
+  region  = "us-central1"
+}
+
+# Ativar o Firebase Authentication
+resource "firebase_project_location_default" "default" {
+  location_id = "us-central1"
+}
+
+resource "firebase_auth_provider" "email_password" {
+  provider_id = "firebase"
+  display_name = "Email/Password"
+  enabled = true
 }
